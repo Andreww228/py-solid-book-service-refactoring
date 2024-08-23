700cb0fcc6fa1ac7303bfa0cef55464617cff68c
AndrewwSS <clash2clans1one@gmail.com>
2024-08-23 09:42:07 +0300

    solution

---
+++ app/book.py
@@ -0,0 +1,18 @@
+from app.display import DisplayStrategy
+from app.print import PrintStrategy
+from app.serialize import SerializeStrategy
+
+
+class Book:
+    def __init__(self, title: str, content: str):
+        self.title = title
+        self.content = content
+
+    def display(self, display_strategy: DisplayStrategy) -> None:
+        display_strategy.display(self.content)
+
+    def print(self, print_strategy: PrintStrategy) -> None:
+        print_strategy.print(self.title, self.content)
+
+    def serialize(self, serialize_strategy: SerializeStrategy) -> str:
+        return serialize_strategy.serialize(self.title, self.content)
